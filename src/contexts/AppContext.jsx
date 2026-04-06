3951bd9e26eb713650b548b9879de570c6b5cc7a
Mazen Yasser <160043731+SuperTooon@users.noreply.github.com>
2026-04-06 21:22:26 +0200

    Add files via upload

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -11,7 +11,7 @@ export function AppProvider({ children }) {
 
   // Dark Mode State
   const [isDark, setIsDark] = useState(() => {
-    return localStorage.getItem('theme') === 'dark';
+    return localStorage.getItem('theme') !== 'light';
   });
 
   // Current Section State
